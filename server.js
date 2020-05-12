6861e8f7d99a7729e4a6225f9195f52fe7d58b44
LucianoRex <lucianor3x@gmail.com>
2020-05-12 01:21:28 -0300

    test API endpoints

--- server.js
+++ server.js
@@ -2,16 +2,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+var bodyParser = require("body-parser");
 const path = require('path');
 
 const app = express();
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
-
-
-const routes = require('./routes/convenio');
+app.use(bodyParser.json());
 
 // Step 2
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/crenovi', {
@@ -23,15 +18,26 @@ mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!!!!');
 });
 
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port", port);
+});
+
+
+const routes = require('./routes/convenio');
+
+
+
 // Data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Step 3
 
-if (process.env.NODE_ENV === 'dev') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
+
 
 
 // HTTP request logger
@@ -39,6 +45,7 @@ app.use(morgan('tiny'));
 app.use('/convenio', routes);
 
 
-
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
 
 //app.listen(PORT, console.log(`Server is starting at ${PORT}`));
